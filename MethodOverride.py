c70a982b5a033f11fffc9efa354b5e5627d915ee
jigarthakkar123 <jigar.thakkar.tops@gmail.com>
2023-06-30 11:54:39 +0530

    30-6-23

---
+++ MethodOverride.py
@@ -0,0 +1,18 @@
+class A:
+    def show(self):
+        print("Show From A")
+class B(A):
+    def show(self):
+        super().show()
+        print("Show From B")
+class C(A):
+    def show(self):
+        super().show()
+        print("Show From C")
+class D(B,C):
+    def show(self):
+        super().show()
+        print("Show From D")
+
+d1=D()
+d1.show()
